cb803c255c18f2fcbcf30798cf3502dc13ec5d1f
Raheel0007 <raheel2535@gmail.com>
2021-01-27 20:01:24 -0600

    Added database

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
 var ball;
+var database;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position')
+    ballPosition.on("value",readPosition);
+
+}
+function readPosition(data){
+position = data.val();
+ball.x = position.x
+ball.y = position.y
+
+
+
+
 }
 
 function draw(){
@@ -21,9 +35,14 @@ function draw(){
         changePosition(0,+1);
     }
     drawSprites();
+
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+database.ref('ball/position').set({
+
+    'x':position.x+x,
+    'y':position.y+y
+    
+})   
 }
